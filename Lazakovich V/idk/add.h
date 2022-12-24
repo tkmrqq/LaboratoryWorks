0bcf8bd863bf91dac81d66856d6ca0d12539b8af
Vladislav <vladislavkurakov2004@gmail.com>
2022-12-24 13:18:50 +0300

    deliting test projects

--- Lazakovich V/idk/add.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <malloc.h>
-
-int str_length(char* str);
-int size_min_word(char* str);
-void print_string(char** str, int size);
-void insertion_sort(char** str, int size);
-void copy_arr(argc, string, argv);

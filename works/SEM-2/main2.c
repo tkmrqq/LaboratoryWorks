68fe2048dcc7bf66c001e7925979b6d3d2b11e44
Vladislav <vladislavkurakov2004@gmail.com>
2023-02-21 23:18:17 +0300

    lab

--- works/SEM-2/main2.c
+++ works/SEM-2/main2.c
@@ -8,12 +8,12 @@ char toLower(char in) {
     return in;
 }
 
-struct character {
+typedef struct character_t {
     int hp;
     int atk;
     int armor;
     char *name;
-};
+} character;
 
 /*enum SortField {
     HP,
@@ -21,26 +21,43 @@ struct character {
     ARMOR
 };*/
 
-struct character createCharacter(int hp, int atk, int armor, const char *name) {
-    struct character c;
+character createCharacter(int hp, int atk, int armor, const char *name) {
+    character c;
     c.hp = hp;
     c.atk = atk;
     c.armor = armor;
-    c.name = (char *) malloc(strlen(name) + 1);// выделение памяти для хранения имени
-    strcpy(c.name, name);                      // копирование имени в выделенную память
+    c.name = (char *) malloc(strlen(name) + 1);//выделение памяти для хранения имени
+    strcpy(c.name, name);                      //копирование имени в выделенную память
     return c;
 }
 
-void printCharacter(struct character c) {
-    printf("Name: %s\n", c.name);
-    printf("HP: %d\n", c.hp);
-    printf("Attack: %d\n", c.atk);
-    printf("Armor: %d\n", c.armor);
+void printCharacter(character c[], int n) {
+    for (int i = 0; i < n; ++i) {
+        printf("===============\n");
+        printf("Name: %s\nHP: %d\nAttack: %d\nArmor: %d\n", c[i].name, c[i].hp, c[i].atk, c[i].armor);
+    }
+}
+
+//сравниваем хар-ки
+int compareByHp(character *a, character *b) {
+    return a->hp - b->hp;
+}
+
+int compareByAtk(character *a, character *b) {
+    return a->atk - b->atk;
+}
+
+int compareByArmor(character *a, character *b) {
+    return a->armor - b->armor;
+}
+
+int compareByName(character *a, character *b) {
+    return strcmp(a->name, b->name);
 }
 
 char *getSortField() {
     static char SortField[5];
-    printf("Enter field to sort by (hp, atk, armor, name): ");
+    printf("Enter field to sort by (hp, atk, armor, name):");
     scanf_s("%5s", SortField);
     for (int i = 0; i < 5; ++i) {
         SortField[i] = toLower(SortField[i]);
@@ -56,49 +73,66 @@ int chooseSort() {
     else if (strcmp(SortField, "armor") == 0)
         return 3;
     else if (strcmp(SortField, "name") == 0)
-        printf("You can't sort by name yet :(\n");
+        return 4;
     return -1;
 }
 
-int compare(const void *a, const void *b, int field) {
-    const struct character *charA = (const struct character *) a;
-    const struct character *charB = (const struct character *) b;
-    int result;
+void sortCharacters(character characters[], int numChars) {
+    int field = chooseSort();
     switch (field) {
-        case 1:// сортировка по hp
-            result = charA->hp - charB->hp;
+        case 1: {
+            qsort(characters, numChars, sizeof(character), (int (*)(const void *, const void *)) compareByHp);
             break;
-        case 2:// сортировка по atk
-            result = charA->atk - charB->atk;
+        }
+        case 2: {
+            qsort(characters, numChars, sizeof(character), (int (*)(const void *, const void *)) compareByAtk);
             break;
-        case 3:// сортировка по armor
-            result = charA->armor - charB->armor;
+        }
+        case 3: {
+            qsort(characters, numChars, sizeof(character), (int (*)(const void *, const void *)) compareByArmor);
             break;
-        default:
+        }
+        case 4: {
+            qsort(characters, numChars, sizeof(character), (int (*)(const void *, const void *)) compareByName);
+            break;
+        }
+        default: {
             fprintf(stderr, "Invalid field type\n");
             exit(EXIT_FAILURE);
+        }
     }
-    return result;
 }
 
-void sort_characters(struct character characters[], int numChars) {
-    int field = chooseSort();
-//    qsort(characters, numChars, sizeof(struct character), (int (*)(const void *, const void *)) compare);
-    qsort(characters, numChars, sizeof(struct character), compare, &field);
+void Remove(character characters[], int *len) {
+    char *name = malloc(0);
+    printf("Input name: ");
+    rewind(stdin);
+    gets(name);
 
+    for (int i = 0; i < *len; i++) {
+        if (!strcmp(characters[i].name, name)) {
+
+            for (int j = i; j < *len - 1; j++) {
+                characters[j] = characters[j + 1];
+            }
+            (*len)--;
+            break;
+        }
+    }
+}
+
+void menu() {
+    printf("What do you want from my program?");
 }
 
 int main() {
-    struct character characters[4];
+    character characters[4];
     characters[0] = createCharacter(33465, 1692, 1111, "Hu tao");
     characters[1] = createCharacter(15444, 1281, 591, "Nahida");
     characters[2] = createCharacter(20867, 1347, 1089, "Raiden");
     characters[3] = createCharacter(15324, 2566, 741, "Ganyu");
     int n = sizeof(characters) / sizeof(characters[0]);
-    sort_characters(characters, n);
-    for (int i = 0; i < 4; ++i) {
-        printf("===============\n");
-        printCharacter(characters[i]);
-    }
+    sortCharacters(characters, n);
+    printCharacter(characters, n);
     return 0;
 }

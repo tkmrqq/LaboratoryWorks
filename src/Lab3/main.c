d659b2884425bf684b26cb4ad0f60279a59fcc42
Vladislav <vladislavkurakov2004@gmail.com>
2023-04-05 01:35:40 +0300

    2

--- src/Lab3/main.c
+++ src/Lab3/main.c
@@ -1,33 +1,32 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <string.h>
 
 #pragma pack(1)
 
 typedef struct {
-    unsigned int type;
-    unsigned int size;
-    unsigned int reversed1, reversed2;
-    unsigned int offset;
+    unsigned char type1, type2;
+    unsigned long size;
+    unsigned short reversed1, reversed2;
+    unsigned long offset;
 } Header;
 
 typedef struct {
     unsigned int Size;
     int Width, Height;
-    unsigned int Planes;
-    unsigned int BitsPerPixel;
+    unsigned short Planes;
+    unsigned short BitsPerPixel;
     unsigned int Compression;
     unsigned int ImageSize;
     int XPixelPerMeter, YPixelPerMeter;
     unsigned int ColorUsed;
 } InfoHeader;
 
-typedef struct {
-    unsigned char r, g, b, junk;
-} COLORINDEX;
 
-int check(int x){
-    while (scanf_s("%d", &x) != 1 ){
+
+int check(int x) {
+    while (scanf_s("%d", &x) != 1) {
         rewind(stdin);
         fprintf(stderr, "ERROR!");
     }
@@ -47,7 +46,7 @@ void convertToBW(int height, int width, unsigned char *pixels) {
             unsigned char r = pixels[index + 2];
             unsigned char g = pixels[index + 1];
             unsigned char b = pixels[index];
-            unsigned char gray = 0.299 * r + 0.587 * g + 0.114 * b;
+            unsigned char gray = (char)(0.299 * r + 0.587 * g + 0.114 * b);
             pixels[index + 2] = gray;
             pixels[index + 1] = gray;
             pixels[index] = gray;
@@ -62,18 +61,18 @@ void gammaCorrection(int height, int width, unsigned char *pixels) {
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             int index = (y * width + x) * 3;
-            double r = pow((double)pixels[index+2] / 255.0, gamma) * 255.0;
-            double g = pow((double)pixels[index+1] / 255.0, gamma) * 255.0;
-            double b = pow((double)pixels[index] / 255.0, gamma) * 255.0;
-            pixels[index + 2] = (unsigned char)r;
-            pixels[index + 1] = (unsigned char)g;
-            pixels[index] = (unsigned char)b;
+            double r = pow((double) pixels[index + 2] / 255.0, gamma) * 255.0;
+            double g = pow((double) pixels[index + 1] / 255.0, gamma) * 255.0;
+            double b = pow((double) pixels[index] / 255.0, gamma) * 255.0;
+            pixels[index + 2] = (unsigned char) r;
+            pixels[index + 1] = (unsigned char) g;
+            pixels[index] = (unsigned char) b;
         }
     }
 }
 
 void menu(int height, int width, unsigned char *pixels) {
-    printf("1)Black and White\n2)Negative\n3)NIGGER\n4)Gamma-correction\n");
+    printf("1)Black and White\n2)Negative\n3)B&W+Neg\n4)Gamma-correction\n");
     int key = 0;
     key = check(key);
     switch (key) {
@@ -95,13 +94,38 @@ void menu(int height, int width, unsigned char *pixels) {
     }
 }
 
+char *getFilePath() {
+    char buffer[256];
+    char *filePath = NULL;
+    size_t filePathLen;
+
+    while (1) {
+        printf("Input file's path:");
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+            printf("Can't read file!\n");
+            exit(EXIT_FAILURE);
+        }
+        char *token = strtok(buffer, "\n");
+        if (strcmp(token + strlen(token) - 4, ".bmp") != 0) {
+            printf("The file must have a .bmp extension\n");
+            continue;
+        }
+        filePathLen = strlen(token) + 1;
+        filePath = (char *) malloc(filePathLen);
+        strncpy(filePath, token, filePathLen);
+        break;
+    }
+
+    return filePath;
+}
+
 int main() {
-    FILE *file = fopen("../sample.bmp", "rb");
+    char *fileName = getFilePath();
+    FILE *file = fopen(fileName, "rb");
     if (file == NULL) {
         printf("Unable to open file.\n");
         return -1;
     }
-    int blockSize = 1024;
     Header fileHeader;
     InfoHeader infoHeader;
     fread(&fileHeader, sizeof(fileHeader), 1, file);
@@ -110,16 +134,18 @@ int main() {
     int width = infoHeader.Width;
     int height = infoHeader.Height;
     int padding = (4 - (width * 3) % 4) % 4;
-    unsigned char* pixels = (unsigned char*)malloc(width * height * 3);
+    printf("%d", width);
+    unsigned char *pixels = (unsigned char *) malloc((int)(width * height * 10));
 
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
-        {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
             int index = (y * width + x) * 3;
             fread(&pixels[index + 2], 1, 1, file);
             fread(&pixels[index + 1], 1, 1, file);
             fread(&pixels[index], 1, 1, file);
+//            if(y == 0) printf("%d", pixels[index + 2]);
+//            if(y == 0) printf("%d", pixels[index + 1]);
+//            if(y == 0) printf("%d\n", pixels[index]);
         }
         fseek(file, padding, SEEK_CUR);
     }
@@ -130,7 +156,7 @@ int main() {
     FILE *outputFile = fopen("../output.bmp", "wb");
     fwrite(&fileHeader, sizeof(fileHeader), 1, outputFile);
     fwrite(&infoHeader, sizeof(infoHeader), 1, outputFile);
-    
+
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             int index = (y * width + x) * 3;
@@ -139,7 +165,7 @@ int main() {
             fwrite(&pixels[index], 1, 1, outputFile);
         }
         for (int i = 0; i < padding; i++) {
-            fputc(0x00, outputFile);
+            fputc(0, outputFile);
         }
     }
     fclose(outputFile);
